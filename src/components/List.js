2c5460dff8534372e1f91d28bb0f836646bcfdc5
Will Locklier <will.locklier@gmail.com>
2020-12-10 11:42:22 -0500

    [List] Add class component

---
+++ src/components/List.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class List extends Component {
+    constructor(props) {
+        super()
+        this.state {
+            
+        }
+
+    }
+
+
+    render () {
+        return (
+            <div>
+                <h1>List</h1>
+            </div>
+        )
+    }
+
+
+}
+
+export default List
